d3cb46102e8caf892cdd51998ec43797672386f2
juanstb <juanjp001566@gmail.com>
2025-11-06 11:12:11 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>El demonio</title>
+    <link rel="stylesheet" href="estilos.css"> </head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<body>
+
+    <header>
+        <h1>El demonio de la tinta</h1>
+        <nav>
+            <a href="#acerca">Acerca de Mí</a>
+            <a href="#portafolio">Portafolio</a>
+            <a href="#estilos">Estilos</a>
+            <a href="#contacto">Contacto</a>
+        </nav>
+    </header>
+
+    <section id="acerca">
+        <h2>Acerca de Mí</h2>
+        <p>El demonio de la tinta es un artista del tatuaje con más de 8 años de experiencia, dedicado a transformar ideas en arte corporal duradero. Mi filosofía se centra en la conexión entre la visión del cliente y la ejecución técnica, asegurando que cada pieza sea tan única como la persona que la lleva.</p>
+        <blockquote id="filosofia">"Cambiando la piel, una obra de arte a la vez."</blockquote>
+    </section>
+
+    <section id="estilos">
+        <h2>Mis Estilos</h2>
+        <p>Explora las técnicas que domino. Haz clic para ver ejemplos.</p>
+        <div class="lista-estilos">
+            <div class="estilo">Realismo Blanco y Negro</div>
+            <div class="estilo">Blackwork / Dotwork</div>
+            <div class="estilo">Neotradicional</div>
+        </div>
+    </section>
+
+    <section id="portafolio">
+        <h2>Galería de Tatuajes</h2>
+        <div class="galeria-grid">
+            <figure class="tatuaje-item">
+                <img src="img/tinta.jpg" alt="Descripción del tatuaje, estilo Realismo">
+                <figcaption>Lobo, Realismo. Zona: Antebrazo.</figcaption>
+            </figure>
+            
+            <figure class="tatuaje-item">
+                <img src="img/tinta.jpg" alt="Descripción del tatuaje, estilo Blackwork">
+                <figcaption>Mándala, Blackwork. Zona: Espalda.</figcaption>
+            </figure>
+            
+            </div>
+    </section>
+
+    <footer id="contacto">
+        <p>© 2025 Santiago molina. <a href="https://wa.me/3137889267?text=Quiero%20cotizar%20mi%20cita.">Contacto</a></p>
+        <div class="redes-sociales">
+            <a href="https://www.instagram.com/_el_demonio_de_la_tinta_" target="_blank" aria-label="Visita nuestro perfil de Instagram">
+            <i class="fab fa-instagram"></i> 
+            </a>
+        </div>
+    </footer>
+
+</body>
+</html>
